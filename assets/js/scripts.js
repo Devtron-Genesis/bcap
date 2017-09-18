29e4b3437c91fd48c963328f0b76dc889cf00679
Gulzaib Tahir <gulzaib.tahir@devtrongenesis.com>
2017-09-19 04:38:03 +0500

    Home page animation

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -145,15 +145,31 @@ $(document).on("scroll", function() {
   }
 });
 
-//  var y = $(window).height();
-//     console.log($('.home').height());
-//     $('.banner').css('height', y - 1);
-//     $('a.scroll_prompt').css('top', y - 0);
-//     console.log(y);
+$(document).ready(function(){
 
-// $("a.scroll_prompt").click(function() {
-//       $('html,body').animate({
-//         scrollTop: $("a.scroll_prompt").offset().top
-//       }, 'slow');
-//     });
+// Create a clone of the menu, right next to original.
+$('ul#hash_menu').addClass('original').clone().insertAfter('ul#hash_menu').addClass('cloned').css('position','fixed').css('top','0').css('margin-top','0').css('z-index','500').removeClass('original').hide();
 
+// scrollIntervalID = setInterval(stickIt, 10);
+
+$('.email_form_field .btn_1').attr("value", $.parseHTML("&#xf054;")[0].data).css('font-size', '17px');
+
+});
+function stickIt() {
+  var orgElementPos = $('.original').offset();
+  orgElementTop = orgElementPos.top;
+  if ($(window).scrollTop() >= (orgElementTop)) {
+    // scrolled past the original position; now only show the cloned, sticky element.
+    // Cloned element should always have same left position and width as original element.
+    orgElement = $('.original');
+    coordsOrgElement = orgElement.offset();
+    leftOrgElement = coordsOrgElement.left;
+    widthOrgElement = orgElement.css('width');
+    $('.cloned').css('left', leftOrgElement + 'px').css('top', 50).css('width', widthOrgElement).show();
+    $('.original').css('visibility', 'hidden');
+  } else {
+    // not scrolled past the menu; only show the original menu.
+    $('.cloned').hide();
+    $('.original').css('visibility', 'visible');
+  }
+}
