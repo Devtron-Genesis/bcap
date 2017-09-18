442da6785bc3d1d69914a589cbc13e84a6a7f258
Shahzad Anwar <shahzad.anwar@devtrongenesis.com>
2017-09-19 02:52:18 +0500

    Owl Carousal Settings

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -38,6 +38,7 @@ $(function() {
     items: 3,
     nav: true,
     dots: false,
+    navText : ["<i class='fa fa-chevron-left'></i>","<i class='fa fa-chevron-right'></i>"],
     responsive: {
       0: {
         items: 1 //Mobile
