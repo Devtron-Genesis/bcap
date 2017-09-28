5ccbac05e24a58b9fab14788b14df0a96ab55ad0
Gulzaib Tahir <gulzaib.tahir@devtrongenesis.com>
2017-09-28 10:38:34 +0500

    final Desktop

--- wp-content/themes/bcapitalgroup/assets/js/scripts.js
+++ wp-content/themes/bcapitalgroup/assets/js/scripts.js
@@ -135,7 +135,7 @@ $(function() {
       target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
       if (target.length) {
         $('html, body').animate({
-          scrollTop: target.offset().top +50
+          scrollTop: target.offset().top + 0
         }, 1000);
         e.preventDefault();
       }
